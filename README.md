1595d0c8d8fe2a2ea92d47d07c21b86a1c2a2fc8
Scott Mackenzie <Scott.Mackenzie@myfoodbag.co.nz>
2020-03-18 11:18:09 +1300

    readme update

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # PSStart
 Powershell start script
+
+After you restart your computer there are usually a bunch of programs you want to run to get started.
+
+This looks to simply configure programs to run.
--- Start.ps1
+++ Start.ps1
@@ -1,18 +1,14 @@
-# if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 
 $configFile = join-path $pwd start.json
 $config = Get-Content $configFile | Out-String | ConvertFrom-Json
-
-function RunLink ($path){
-    Invoke-Item -Path $path
-}
 
 push-location $pwd
 cd 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs'
 $config.tasks | foreach {
     write-host $_.name
     if ($_.type -eq 'lnk') {
-        RunLink($_.path)
+        Invoke-Item -Path $_.path
     }
     if ($_.type -eq 'process') {
         iex $_.cmd
